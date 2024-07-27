bc18c37b6240cb0ebed07efe13adee240218cb71
Emad <diyanat00emad@gmail.com>
2024-07-27 14:51:10 +0330

    Add project files.

---
+++ HMI DIGITAL SAMPLE/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Threading;
+using System.Windows.Threading;
+using Timer = System.Threading.Timer;
+
+namespace HMI_DIGITAL_SAMPLE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
